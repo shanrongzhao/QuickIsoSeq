10f6cbf57619dd39313ccd9ce03b1adf68ce34a6
shanrongzhao <59151349+shanrongzhao@users.noreply.github.com>
2020-01-29 09:02:56 -0500

    Update master-cmd.sh

--- test_run/master-cmd.sh
+++ test_run/master-cmd.sh
@@ -1,7 +1,10 @@
+#
+# Step #1
+# Process individual samples
+#
 export QuickIsoSeq=/lustre/workspace/projects/ECD/zhaos25/QuickIsoSeq_Pub
 export PATH=$QuickIsoSeq:$PATH
 
-#Step #1
 run-isoseq.sh allIDs.txt run.config
 
 
@@ -9,10 +12,9 @@ run-isoseq.sh allIDs.txt run.config
 # Step #2
 # Summarization   Note: run this step after Step #1 is done
 #
-#
-ml RHEL6-apps R/3.2.3
-
 export QuickIsoSeq=/lustre/workspace/projects/ECD/zhaos25/QuickIsoSeq_Pub
+export PATH=$QuickIsoSeq:$PATH
+ml RHEL6-apps R/3.2.3
 
 merge-isoseq.sh allIDs.txt  run.config &> Results.log
 #bsub -q short -app large "merge-isoseq.sh allIDs.txt  run.config &> Results.log"
