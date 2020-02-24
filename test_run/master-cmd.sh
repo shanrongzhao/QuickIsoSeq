cacd4bf352dc7b3df87f950701f6242c6ac46753
shanrongzhao <59151349+shanrongzhao@users.noreply.github.com>
2020-02-23 20:46:19 -0500

    Add files via upload

--- test_run/master-cmd.sh
+++ test_run/master-cmd.sh
@@ -2,7 +2,7 @@
 # Step #1
 # Process individual samples
 #
-export QuickIsoSeq=/lustre/workspace/projects/ECD/zhaos25/QuickIsoSeq_Pub
+export QuickIsoSeq=/lustre/workspace/projects/ECD/zhaos25/QuickIsoSeqPro
 export PATH=$QuickIsoSeq:$PATH
 
 run-isoseq.sh allIDs.txt run.config
@@ -12,9 +12,11 @@ run-isoseq.sh allIDs.txt run.config
 # Step #2
 # Summarization   Note: run this step after Step #1 is done
 #
-export QuickIsoSeq=/lustre/workspace/projects/ECD/zhaos25/QuickIsoSeq_Pub
+export QuickIsoSeq=/lustre/workspace/projects/ECD/zhaos25/QuickIsoSeqPro
 export PATH=$QuickIsoSeq:$PATH
-ml RHEL6-apps R/3.2.3
+
+#make sure your R envorionment is available. Below is speicifc for Pfizer's environment
+ml RHEL6-apps R/3.2.3   
 
 merge-isoseq.sh allIDs.txt  run.config &> Results.log
 #bsub -q short -app large "merge-isoseq.sh allIDs.txt  run.config &> Results.log"
